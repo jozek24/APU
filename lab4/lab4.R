f43ee4631a8541745b4b46632a91f469d1e5d3d6
jozek24 <47977468+jozek24@users.noreply.github.com>
2022-02-05 14:52:22 +0100

    lab4

---
+++ lab4/lab4.R
@@ -0,0 +1,20 @@
+install.packages("C50")
+
+require(C50)
+
+motorola <- read.csv("C:/Users/jozek/OneDrive/Studia - semestr 1/Analiza Procesów Uczenia/apu/apu/lab4/motorola.csv")
+
+data <- motorola[-c(1,7,10)]
+
+str(data)
+
+data[ ,'pamiec_RAM'] <- factor(data[ ,'pamiec_RAM'])
+
+data[0:6]
+
+m1 <- C5.0 (data[0:6],factor(data$ocena_klienta))
+
+summary(m1)
+
+plot(m1)
+
